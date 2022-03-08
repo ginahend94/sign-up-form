d00f0264b4d6205bdffebe82947f99b413e97180
Gina Henderson <gina@ginahenderson.me>
2022-03-08 15:22:37 -0600

    Validity

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll('input');
+//inputs.forEach(a => a.addEventListener('click', e => console.log(e.target.validity))); //Test
+inputs.forEach(a => a.addEventListener('change', e => {
+    if (e.target.validity.valid) {
+        e.target.classList.add('valid');
+        e.target.classList.remove('invalid')
+    }
+    else {
+        e.target.classList.remove('valid');
+        e.target.classList.add('invalid')
+    }
+    console.log(e.target.classList);
+}))
